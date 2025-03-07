e16e7ea67440b8a39ecd19c4a27a7b66a2ad12d1
pattaraponssky <pattarapon.ssk.y@gmail.com>
2025-03-07 12:02:47 +0700

    delete ras

--- backend/hec_ras_compute.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-python D:\sti_wangyang\hec_ras_compute.py
-IF %ERRORLEVEL% EQU 0 (
-    echo Success Run
-) ELSE (
-    echo Error: Script failed with exit code %ERRORLEVEL%.
-)
-pause
--- backend/hec_ras_update_date.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-python D:\sti_wangyang\ras_update_date.py
-IF %ERRORLEVEL% EQU 0 (
-    echo Success Run
-) ELSE (
-    echo Error: Script failed with exit code %ERRORLEVEL%.
-)
-pause
